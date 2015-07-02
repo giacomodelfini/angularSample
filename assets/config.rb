874008165edc73c98710c81e8eae8682efbcf858
dgiac85 <dgiac85@gmail.com>
2015-07-02 17:32:11 +0200

    primo commit

---
+++ assets/config.rb
@@ -0,0 +1,8 @@
+#This file contains the COMPASS watcher configuration
+#for the SCSS file compilation. */
+
+require 'compass/import-once/activate'
+#root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
